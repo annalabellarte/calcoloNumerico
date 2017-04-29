e0ec4dcfb6d5b9732b55f7f0481be64c96a4973d
Anna Labellarte <anna.labellarte.al@gmail.com>
2017-04-29 22:25:42 +0200

    first commit

---
+++ sistemi_triangolari.m
@@ -0,0 +1,34 @@
+A=[7 4 3 8;0 1 2 5;0 0 1 7;0 0 0 5];
+b=[2 3 7 10];
+risultato=sistemi_triangolari_indietro(A,b);
+function x=sistemi_triangolari_indietro(A, b)
+    n = length(b);
+    %controlli sull'input
+    if(n~=length(A))
+        error("errore 1");
+    end
+    if(abs(A(n,n)<eps))
+        error('La matrice non e'' singolare');
+    end
+    [j,k] = size(A);[m,n] = size(A);
+    if(j ~=k)
+        error('La matrice non e'' quadrata');
+    end
+    if(~triu(A))
+        error('A deve essere triangolare superiore');
+    end
+    
+    x(n)= b(n)/A(n,n);
+    for(i=n-1:-1:1)
+        x(i)=b(i);
+        for(j=i+1:n)
+           x(i)= (x(i)-(A(i,j)*x(j)));
+        end
+        if(abs(A(n,n)>eps))
+        x(i) =x(i)/A(i,i);
+        else
+            error('matrice s>ingolare');
+        end
+    end
+  return  
+end
